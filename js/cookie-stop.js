aff576e45face09d2d5567a3cf86a6b49fbfaac1
somtam <lorenzonovia@gmail.com>
2015-07-02 20:45:13 +0700

    Insert cookie after scroll > 100px

--- js/cookie-stop.js
+++ js/cookie-stop.js
@@ -3,6 +3,7 @@
     var d = window.document;
     var cookieName = "cookieStopAccept";
     var cookieNameId = "ck-stop";
+    var btAccept = "cookieStopBt";
     var cookieSetup = false;
 
     var cookieStop = {
@@ -21,7 +22,6 @@
                 this.bgBorder = '#999';
                 this.textColour = '#333';
             }
-
             this.cookieMain();
         },
         cookieMain: function () {
@@ -35,6 +35,8 @@
             }
         },
         checkCookie: function (cname) {
+            // Display the header only if the cookie has not been set.
+            //return d.cookie.match(new RegExp(cookieName + '=([^;]+)'));
 
             var name = cname + "=";
             var ca = document.cookie.split(';');
@@ -50,13 +52,13 @@
         },
         createBar: function () {
             var barStyle = 'position:fixed; width:100%; background-color:' + this.bg + '; color:' + this.textColour +
-                    '; margin:0; left:0; line-height:2; border-bottom:1px solid ' + this.bgBorder + ';' +
-                    ' top:0; padding:15px 5px; z-index:1000; text-align:center;';
+                    '; margin:0; left:0; line-height:2.5; border-bottom:1px solid ' + this.bgBorder + ';' +
+                    ' top:0; padding:15px 20px; z-index:1000; text-align:center;';
             var bar = d.createElement('div');
             bar.id = cookieNameId;
             bar.style.cssText = barStyle;
             bar.innerHTML = this.cnt + ' <a href="#" id="cookieStopAccept" style="' +
-                    'background-color:#f2f2f2; padding:8px 8px; text-decoration:none; ' +
+                    'background-color:#f2f2f2; padding:5px 5px; text-decoration:none; ' +
                     ' -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px;color:#333; margin-left:20px;">' +
                     this.btok + '</a> <a href="' + this.link + '" style="color:#666; margin-left:20px">' +
                     this.btinfo + '</a>';
@@ -67,7 +69,12 @@
         },
         addEventsForCookie: function () {
             d.onclick = this.afterAccept;
-            d.onscroll = this.afterAccept;
+            d.onscroll = this.checkScrollAmount;
+        },
+        checkScrollAmount: function(){
+		    if( window.scrollY > 100){
+		        window.cookieStop.afterAccept();
+			}  
         },
         afterAccept: function () {
             if (cookieSetup === false) {
@@ -76,21 +83,19 @@
                 cookieSetup = true;
             }
         }
-
     };
     window.cookieStop = cookieStop;
 
     function insertCookie() {
+        // Set the cookie expiry to one year after today.
         var expiryDate = new Date();
         expiryDate.setFullYear(expiryDate.getFullYear() + 1);
         document.cookie = cookieName + '=y; expires=' + expiryDate.toGMTString() + '; path=/';
     }
-	
+
     function activateServices() {
         var bar = d.getElementById(cookieNameId);
         bar.parentNode.removeChild(bar);
-        
-        // insert other stuff you want to add after the cookie is set
     }
 
 })(this);
